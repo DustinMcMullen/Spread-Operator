c6db02766a7bc53d40891563c2b50756a8c00ca6
DustinMcMullen <51675499+DustinMcMullen@users.noreply.github.com>
2020-09-05 11:23:18 -0500

    Shortened code using spread operator Utilized spread operator within setContact function of useState for contact to remove if/else statements & vastly shorten code. Inserted the whole previous value for contact & replaced only portion needed in one statement.

--- src/index.js
+++ src/index.js
@@ -3,18 +3,3 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
-
-var citrus = ["lemon", "lime", "orange", "grapefruit"];
-var fruit = ["apple", "banana", "watermellon", ...citrus];
-
-const fullName = {
-  firstName: "James",
-  lastName: "Bond"
-};
-
-const user = {
-  ...fullName,
-  id: "007",
-  weapon: "Golden Gun"
-};
-console.log(user);
